c60e11eef90353bd21996ee2213cf725d2ba14ae
Dev750wala <sadisatsowaladev1@gmail.com>
2024-12-25 03:19:32 +0530

    feat: make price feed public and add constructor test for aggregator address

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -22,7 +22,7 @@ contract FundMe {
     address private immutable i_owner;
     address[] private s_funders;
     mapping(address => uint256) private s_addressToAmountFunded;
-    AggregatorV3Interface private s_priceFeed;
+    AggregatorV3Interface public s_priceFeed;
 
     // Events (we have none!)
 
--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -1,14 +1,32 @@
+import { assert } from "chai";
+import { Contract } from "ethers";
 import { deployments, ethers, getNamedAccounts } from "hardhat"
 
 describe("FundMe", () => {
-    let fundMe
+    let fundMe: Contract, MockV3Aggregator: Contract, deployer: string;
     beforeEach(async function () {
-        const { deployer } = await getNamedAccounts()
+        deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(["all"])
         const deployerSigner = await ethers.getSigner(deployer)
 
-        // ethers.getSigners() function returns an array of signers, of which private keys we have specified in the hardhat.config.ts file.
+        // NOTE: ethers.getSigners() function returns an array of signers, of which private keys we have specified in the hardhat.config.ts file .
 
-        fundMe = await ethers.getContractAt("FundMe", (await deployments.get("FundMe")).address, deployerSigner)
+        fundMe = await ethers.getContractAt(
+            "FundMe", 
+            (await deployments.get("FundMe")).address, 
+            deployerSigner
+        );
+        MockV3Aggregator = await ethers.getContractAt(
+            "MockV3Aggregator", 
+            (await deployments.get("MockV3Aggregator")).address, 
+            deployerSigner
+        );
+    }); 
+
+    describe("constructor", async function () {
+        it("It sets aggregator addresses correctly", async function () {
+            const priceFeed = await fundMe.s_priceFeed()
+            assert.equal(priceFeed, await MockV3Aggregator.getAddress())
+        })
     })
 })

8f612166f1adce254b62691429c56f86ef79f2e2
Dev750wala <sadisatsowaladev1@gmail.com>
2024-12-26 05:01:35 +0530

    feat: enable gas reporter and add token configuration for gas reporting

---
+++ ignition/modules/Fund.ts
@@ -0,0 +1,13 @@
+// import { getNamedAccounts } from "hardhat"
+
+// async function main() {
+//     const {  } = await 
+// }
+
+
+// main()
+//     .then(() => process.exit(0))
+//     .catch(error => {
+//         console.error(error);
+//         process.exit(1);
+//     });

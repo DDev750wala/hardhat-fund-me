cd0425fa19019649f527ef39f9c66bdd3ced1cd7
Dev750wala <sadisatsowaladev1@gmail.com>
2024-12-27 08:31:02 +0530

    feat: implement funding script for FundMe contract

--- scripts/Fund.ts
+++ scripts/Fund.ts
@@ -1,1 +1,20 @@
-import { getNamedAccounts } from "hardhat"
+import { ethers, getNamedAccounts } from "hardhat"
+
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContractAt("FundMe", deployer)
+    console.log(`Got contract FundMe at ${fundMe.address}`)
+    console.log("Funding contract...")
+    const transactionResponse = await fundMe.fund({
+        value: ethers.parseEther("0.1"),
+    })
+    await transactionResponse.wait()
+    console.log("Funded!")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })

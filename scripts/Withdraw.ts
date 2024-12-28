9adb5f5ab1fc505eb0a0346877b6b2c38fd31e6d
Dev750wala <sadisatsowaladev1@gmail.com>
2024-12-28 09:26:29 +0530

    feat: add withdraw script and chain ID configuration for local deployment

--- scripts/Withdraw.ts
+++ scripts/Withdraw.ts
@@ -5,18 +5,26 @@ import { TransactionResponse } from "ethers";
 async function main() {
     const deployer = await getNamedAccounts()
     const deployerSigner= await ethers.getSigner(deployer.deployer)
-
     const fundMe: FundMe = await ethers.getContractAt(
         "FundMe", 
         (await deployments.get("FundMe")).address,
         deployerSigner
-        ) as unknown as FundMe;
-
-    console.log("Funding contract...");
+    ) as unknown as FundMe;
+    
+    const contractBalance = await ethers.provider.getBalance(fundMe.getAddress());
+    console.log(`Contract balance: ${contractBalance.toString()}`);
+    
+    console.log("Funding...");
 
     const transactionResponse: TransactionResponse = await fundMe.cheaperWithdraw();
     await transactionResponse.wait(1);
     console.log("Got the money back!");
-    
-    
 }
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
